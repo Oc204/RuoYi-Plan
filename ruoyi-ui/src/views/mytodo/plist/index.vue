f44e561b62865558b4286dc7bd2164eb8faefeef
oc204 <oc204@qq.com>
2022-03-08 01:28:34 +0800

    fix: 修改bug

--- ruoyi-ui/src/views/mytodo/plist/index.vue
+++ ruoyi-ui/src/views/mytodo/plist/index.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="计划id" prop="planId">
+    <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch">
+      <el-form-item label="清单名称" prop="listName">
         <el-input
-          v-model="queryParams.planId"
-          placeholder="请输入计划id"
+          v-model="queryParams.listName"
+          placeholder="请输入清单名称"
           clearable
           size="small"
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="清单id
-" prop="listId">
-        <el-input
-          v-model="queryParams.listId"
-          placeholder="请输入清单id
-"
-          clearable
-          size="small"
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
+<!--      <el-form-item label="状态" prop="status">-->
+<!--        <el-select v-model="queryParams.status" placeholder="菜单状态" clearable size="small">-->
+<!--          <el-option-->
+<!--            v-for="dict in dict.type.sys_normal_disable"-->
+<!--            :key="dict.value"-->
+<!--            :label="dict.label"-->
+<!--            :value="dict.value"-->
+<!--          />-->
+<!--        </el-select>-->
+<!--      </el-form-item>-->
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
         <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
@@ -35,86 +34,109 @@
           icon="el-icon-plus"
           size="mini"
           @click="handleAdd"
-          v-hasPermi="['system:plist:add']"
+          v-hasPermi="['system:menu:add']"
         >新增</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="success"
+          type="info"
           plain
-          icon="el-icon-edit"
+          icon="el-icon-sort"
           size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['system:plist:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['system:plist:remove']"
-        >删除</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="el-icon-download"
-          size="mini"
-          @click="handleExport"
-          v-hasPermi="['system:plist:export']"
-        >导出</el-button>
+          @click="toggleExpandAll"
+        >展开/折叠</el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="plistList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="${comment}" align="center" prop="id" />
-      <el-table-column label="计划id" align="center" prop="planId" />
-      <el-table-column label="清单id" align="center" prop="listId" />
+    <el-table
+      v-if="refreshTable"
+      v-loading="loading"
+      :data="pListList"
+      highlight-current-row
+      row-key="id"
+      :default-expand-all="isExpandAll"
+      :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+    >
+      <el-table-column prop="listName" label="清单名称" :show-overflow-tooltip="true" width="160"></el-table-column>
+      <el-table-column prop="weight" label="排序" width="60"></el-table-column>
+      <el-table-column prop="remark" label="备注" :show-overflow-tooltip="true"></el-table-column>
+      <el-table-column label="创建时间" align="center" prop="createTime">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.createTime) }}</span>
+        </template>
+      </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button
             size="mini"
             type="text"
+            icon="el-icon-search"
+            @click="handleDetail(scope.row)"
+            v-hasPermi="['system:menu:edit']"
+          >查看任务</el-button>
+          <el-button
+            size="mini"
+            type="text"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
-            v-hasPermi="['system:plist:edit']"
+            v-hasPermi="['system:menu:edit']"
           >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-plus"
+            @click="handleAdd(scope.row)"
+            v-hasPermi="['system:menu:add']"
+          >新增</el-button>
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
-            v-hasPermi="['system:plist:remove']"
+            v-hasPermi="['system:menu:remove']"
           >删除</el-button>
         </template>
       </el-table-column>
     </el-table>
 
-    <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="queryParams.pageNum"
-      :limit.sync="queryParams.pageSize"
-      @pagination="getList"
-    />
+    <!-- 添加或修改菜单对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="680px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="100px">
+        <el-row>
+          <el-col :span="24">
+            <el-form-item label="上级菜单">
+              <treeselect
+                v-model="form.parentId"
+                :options="pListOptions"
+                :normalizer="normalizer"
+                :show-count="true"
+                placeholder="选择上级菜单"
+              />
+            </el-form-item>
+          </el-col>
 
-    <!-- 添加或修改计划清单关联对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="计划id" prop="planId">
-          <el-input v-model="form.planId" placeholder="请输入计划id" />
-        </el-form-item>
-        <el-form-item label="清单id" prop="listId">
-          <el-input v-model="form.listId" placeholder="请输入清单id" />
-        </el-form-item>
+          <el-col :span="24">
+            <el-form-item label="清单名称" prop="listName">
+              <el-input v-model="form.listName" placeholder="请输入清单名称" />
+            </el-form-item>
+          </el-col>
+          <!--        <el-form-item label="用户id" prop="userId">-->
+          <!--          <el-input v-model="form.userId" placeholder="请输入用户id" />-->
+          <!--        </el-form-item>-->
+          <el-col :span="24">
+            <el-form-item label="备注" prop="remark">
+            <el-input type="textarea" v-model="form.remark" placeholder="请输入备注" />
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="12">
+            <el-form-item label="显示排序" prop="weight">
+              <el-input-number v-model="form.weight" controls-position="right" :min="0" />
+            </el-form-item>
+          </el-col>
+
+        </el-row>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button type="primary" @click="submitForm">确 定</el-button>
@@ -125,41 +147,57 @@
 </template>
 
 <script>
-import { listPlist, getPlist, delPlist, addPlist, updatePlist } from "@/api/mytodo/plist";
+
+import {
+  listPlist,
+  getPlist,
+  delPlist,
+  addPlist,
+  updatePlist,
+  listCurrentUserPlist
+} from "@/api/mytodo/plist";
+
+import Treeselect from "@riophae/vue-treeselect";
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
+import IconSelect from "@/components/IconSelect";
 
 export default {
-  name: "Plist",
+  name: "Menu",
+  dicts: ['sys_show_hide', 'sys_normal_disable'],
+  components: { Treeselect, IconSelect },
   data() {
     return {
       // 遮罩层
       loading: true,
-      // 选中数组
-      ids: [],
-      // 非单个禁用
-      single: true,
-      // 非多个禁用
-      multiple: true,
       // 显示搜索条件
       showSearch: true,
-      // 总条数
-      total: 0,
-      // 计划清单关联表格数据
-      plistList: [],
+      // 清单表格树数据
+      pListList: [],
+      // 清单树选项
+      pListOptions: [],
       // 弹出层标题
       title: "",
       // 是否显示弹出层
       open: false,
+      // 是否展开，默认全部折叠
+      isExpandAll: false,
+      // 重新渲染表格状态
+      refreshTable: true,
       // 查询参数
       queryParams: {
-        pageNum: 1,
-        pageSize: 10,
-        planId: null,
-        listId: null,
+        listName: undefined,
+        visible: undefined
       },
       // 表单参数
       form: {},
       // 表单校验
       rules: {
+        weight: [
+          { required: true, message: "显示顺序不能为空", trigger: "blur" }
+        ],
+        listName: [
+          { required: true, message: "清单名称不能为空", trigger: "blur" }
+        ],
       }
     };
   },
@@ -167,13 +205,36 @@ export default {
     this.getList();
   },
   methods: {
-    /** 查询计划清单关联列表 */
+    // 选择图标
+    selected(name) {
+      this.form.icon = name;
+    },
+    /** 查询菜单列表 */
     getList() {
       this.loading = true;
-      listPlist(this.queryParams).then(response => {
-        this.plistList = response.rows;
-        this.total = response.total;
+      listCurrentUserPlist(this.queryParams).then(response => {
+        this.pListList = this.handleTree(response.data, "id");
         this.loading = false;
+      });
+    },
+    /** 转换菜单数据结构 */
+    normalizer(node) {
+      if (node.children && !node.children.length) {
+        delete node.children;
+      }
+      return {
+        id: node.id,
+        label: node.listName,
+        children: node.children
+      };
+    },
+    /** 查询菜单下拉树结构 */
+    getTreeselect() {
+      listCurrentUserPlist().then(response => {
+        this.pListOptions = [];
+        const pList = { id: 0, listName: '主清单', children: [] };
+        pList.children = this.handleTree(response.data, "id");
+        this.pListOptions.push(pList);
       });
     },
     // 取消按钮
@@ -184,16 +245,15 @@ export default {
     // 表单重置
     reset() {
       this.form = {
-        id: null,
-        planId: null,
-        listId: null,
-        createTime: null
+        id: undefined,
+        parentId: 0,
+        listName: undefined,
+        weight: undefined
       };
       this.resetForm("form");
     },
     /** 搜索按钮操作 */
     handleQuery() {
-      this.queryParams.pageNum = 1;
       this.getList();
     },
     /** 重置按钮操作 */
@@ -201,33 +261,48 @@ export default {
       this.resetForm("queryForm");
       this.handleQuery();
     },
-    // 多选框选中数据
-    handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.id)
-      this.single = selection.length!==1
-      this.multiple = !selection.length
+    /**查看清单下的任务操作 */
+    handleDetail(row) {
+      this.$router.push({ path: '/mytodo/mytask/index', query: {
+          plistId: row.id,
+          plistName: row.listName
+        } });
     },
     /** 新增按钮操作 */
-    handleAdd() {
+    handleAdd(row) {
       this.reset();
+      this.getTreeselect();
+      if (row != null && row.id) {
+        this.form.parentId = row.id;
+      } else {
+        this.form.parentId = 0;
+      }
       this.open = true;
-      this.title = "添加计划清单关联";
+      this.title = "添加清单";
+    },
+    /** 展开/折叠操作 */
+    toggleExpandAll() {
+      this.refreshTable = false;
+      this.isExpandAll = !this.isExpandAll;
+      this.$nextTick(() => {
+        this.refreshTable = true;
+      });
     },
     /** 修改按钮操作 */
     handleUpdate(row) {
       this.reset();
-      const id = row.id || this.ids
-      getPlist(id).then(response => {
+      this.getTreeselect();
+      getPlist(row.id).then(response => {
         this.form = response.data;
         this.open = true;
-        this.title = "修改计划清单关联";
+        this.title = "修改清单";
       });
     },
     /** 提交按钮 */
-    submitForm() {
+    submitForm: function() {
       this.$refs["form"].validate(valid => {
         if (valid) {
-          if (this.form.id != null) {
+          if (this.form.id != undefined) {
             updatePlist(this.form).then(response => {
               this.$modal.msgSuccess("修改成功");
               this.open = false;
@@ -245,19 +320,12 @@ export default {
     },
     /** 删除按钮操作 */
     handleDelete(row) {
-      const ids = row.id || this.ids;
-      this.$modal.confirm('是否确认删除计划清单关联编号为"' + ids + '"的数据项？').then(function() {
-        return delPlist(ids);
+      this.$modal.confirm('是否确认删除名称为"' + row.listName + '"的数据项？').then(function() {
+        return delPlist(row.id);
       }).then(() => {
         this.getList();
         this.$modal.msgSuccess("删除成功");
       }).catch(() => {});
-    },
-    /** 导出按钮操作 */
-    handleExport() {
-      this.download('system/plist/export', {
-        ...this.queryParams
-      }, `plist_${new Date().getTime()}.xlsx`)
     }
   }
 };
