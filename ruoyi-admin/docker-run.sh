40d3ac3049c5c11cd952ce802132579abf55bc20
oc204 <oc204@qq.com>
2022-03-22 21:24:56 +0800

    fix: 去掉错误命令

--- ruoyi-admin/docker-run.sh
+++ ruoyi-admin/docker-run.sh
@@ -2,5 +2,5 @@
 
 docker build -t ruoyi-plan:1.0.0 .
 
-docker run -d -p 8080:8080 --name ruoyiplan /home/ruoyi/uploadPath -v /home/ruoyi/uploadPath:/home/ruoyi/uploadPath ruoyi-plan:1.0.0
+docker run -d -p 8080:8080 --name ruoyiplan -v /home/ruoyi/uploadPath:/home/ruoyi/uploadPath ruoyi-plan:1.0.0
 
