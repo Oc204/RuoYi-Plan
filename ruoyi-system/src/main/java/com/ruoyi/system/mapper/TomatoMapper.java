bdd060c47d1e8faff8522e0211071653f4baeb2d
oc204 <oc204@qq.com>
2022-03-27 13:56:20 +0800

    doc: 编辑番茄mapper注释文档

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/TomatoMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/TomatoMapper.java
@@ -69,7 +69,23 @@ public interface TomatoMapper
      */
     Map<String, Object> selectTomatoStatisticsByUserId(Long userId);
 
+    /**
+     * 根据年份获取番茄曲线
+     * @param showYear 年显示标识
+     * @param showMonth 月显示标识
+     * @param userId 用户id
+     * @param year 年份值
+     * @return
+     */
     List<Map<String, Object>> getTomatoLineByYear(@Param("showYear")String showYear, @Param("showMonth")String showMonth, @Param("userId")Long userId, @Param("year")String year);
 
+    /**
+     * 根据时间获取饼图番茄数据
+     * @param showDay   日显示标识
+     * @param showWeek  周显示标识
+     * @param showMonth 月显示标识
+     * @param userId    用户id
+     * @return
+     */
     List<Map<String, Object>> getTomatoPieChartsData(@Param("showDay")String showDay, @Param("showWeek")String showWeek, @Param("showMonth")String showMonth, @Param("userId")Long userId);
 }
